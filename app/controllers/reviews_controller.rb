a55cb855905e5ca404ece80206eda2f80d2f439c
mateopark <mateo900210@gmail.com>
2021-05-29 16:02:46 -0500

    review controller updated

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,33 @@
 class ReviewsController < ApplicationController
+
+   def new
+    @stall = Stall.find(params[:stall_id])
+    @review = Review.new
+  end
+
+   def create
+    @review = Review.new(review_params)
+    @stall = Stall.find(params[:stall_id])
+    @review.stall = @stall
+    @review.user = @current_user
+    if @review.save
+      redirect_to stall_path(@stall)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @stall = @review.stall
+    @review.destroy
+    redirect_to stall_path(@stall)
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:comment, :rating)
+  end
+
 end
