244e92e1fa9df9a8b54c52aebaeda428664b55ef
Nikita Siniauski <nsiniauski@gmail.com>
2021-06-01 21:35:18 -0500

    designed components for review cards, avatars and banner

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
     resources :stalls do
       resources :reviews, only: [:new, :create, :destroy]
     end
+
+  # For testing purposes only:
+  get '/testpage', to: 'pages#compTest'
 end
