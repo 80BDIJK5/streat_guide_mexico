f1a428e8f285edf66453ad31e8218fa945f6a19e
Bob Dijks <rhcdijks@gmail.com>
2021-06-08 21:15:09 -0500

    search is fully working

--- app/models/stall.rb
+++ app/models/stall.rb
@@ -1,4 +1,11 @@
 class Stall < ApplicationRecord
+  include PgSearch::Model
+  pg_search_scope :search_by_name_and_description,
+                  against: [ :name, :description ],
+                  using: {
+                    tsearch: { prefix: true }
+                  }
+
   has_many :reviews
   belongs_to :user
   validates :name, presence: true
